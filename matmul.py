bfe3cc0e4e389dc1042a214d299e21a93ef64ee8
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2025-03-22 22:59:49 +0100

    autotune, we got the flops!

--- matmul.py
+++ matmul.py
@@ -3,6 +3,30 @@ import torch
 import triton
 import triton.language as tl
 
+def get_configs():
+    return [
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64}, num_stages=3,
+                              num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32}, num_stages=4,
+                        num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32}, num_stages=4,
+                        num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32}, num_stages=4,
+                        num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32}, num_stages=4,
+                        num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32}, num_stages=4,
+                        num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32}, num_stages=5,
+                        num_warps=2),
+        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32}, num_stages=5,
+                        num_warps=2),
+    ]
+
+@triton.autotune(
+    configs=get_configs(),
+    key=["m", "n", "k"]
+)
 @triton.jit
 def matmul_kernel(
     a_ptr, # (M, K)
@@ -15,25 +39,29 @@ def matmul_kernel(
     c_m_stride,
     c_n_stride,
     m, k, n,
-    BLOCK_SIZE: tl.constexpr
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_K: tl.constexpr,
 ):
     tile_m = tl.program_id(axis=0)
     tile_n = tl.program_id(axis=1)
 
-    block_idxs = tl.arange(0, BLOCK_SIZE)
-    a_offsets = block_idxs[:, None] * a_m_stride + block_idxs[None, :] * a_k_stride
-    b_offsets = block_idxs[:, None] * b_k_stride + block_idxs[None, :] * b_n_stride
-    c_offsets = block_idxs[:, None] * c_m_stride + block_idxs[None, :] * c_n_stride
+    block_m_idxs = tl.arange(0, BLOCK_SIZE_M)
+    block_n_idxs = tl.arange(0, BLOCK_SIZE_N)
+    block_k_idxs = tl.arange(0, BLOCK_SIZE_K)
+    a_offsets = block_m_idxs[:, None] * a_m_stride + block_k_idxs[None, :] * a_k_stride
+    b_offsets = block_k_idxs[:, None] * b_k_stride + block_n_idxs[None, :] * b_n_stride
+    c_offsets = block_m_idxs[:, None] * c_m_stride + block_n_idxs[None, :] * c_n_stride
 
-    acc = tl.zeros((BLOCK_SIZE, BLOCK_SIZE), dtype=tl.float32)  # accumulate in higher precision
-    for tile_k in tl.range(tl.cdiv(k, BLOCK_SIZE)):
-        a_tile_ptr = a_ptr + tile_m * BLOCK_SIZE * a_m_stride + tile_k * BLOCK_SIZE * a_k_stride
+    acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)  # accumulate in higher precision
+    for tile_k in tl.range(tl.cdiv(k, BLOCK_SIZE_K)):
+        a_tile_ptr = a_ptr + tile_m * BLOCK_SIZE_M * a_m_stride + tile_k * BLOCK_SIZE_K * a_k_stride
         a_tile = tl.load(a_tile_ptr + a_offsets)
-        b_tile_ptr = b_ptr + tile_k * BLOCK_SIZE * b_k_stride + tile_n * BLOCK_SIZE * b_n_stride
+        b_tile_ptr = b_ptr + tile_k * BLOCK_SIZE_K * b_k_stride + tile_n * BLOCK_SIZE_N * b_n_stride
         b_tile = tl.load(b_tile_ptr + b_offsets)
 
         acc = tl.dot(a_tile, b_tile, acc)
-    c_tile_ptr = c_ptr + tile_m * BLOCK_SIZE * c_m_stride + tile_n * BLOCK_SIZE * c_n_stride
+    c_tile_ptr = c_ptr + tile_m * BLOCK_SIZE_M * c_m_stride + tile_n * BLOCK_SIZE_N * c_n_stride
     tl.store(c_tile_ptr + c_offsets, acc.to(tl.float16))
 
 
@@ -42,17 +70,14 @@ def matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     M, K = a.shape
     _, N = b.shape
 
-    BLOCK_SIZE = 16
-
     assert a.dtype == torch.float16 and a.dtype == b.dtype
     c = torch.empty(M, N, dtype=torch.float16, device="cuda:0")
-    grid = (triton.cdiv(M, BLOCK_SIZE), triton.cdiv(N, BLOCK_SIZE))
+    grid = lambda meta: (triton.cdiv(M, meta["BLOCK_SIZE_M"]), triton.cdiv(N, meta["BLOCK_SIZE_N"]))
     matmul_kernel[grid](
         a, a.stride(0), a.stride(1),
         b, b.stride(0), b.stride(1),
         c, c.stride(0), c.stride(1),
         M, N, K,
-        BLOCK_SIZE=BLOCK_SIZE
     )
     return c
 
@@ -69,6 +94,7 @@ def matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     )
 )
 def benchmark(N: int, provider: Literal["torch"] | Literal["triton"]):
+    M, N, K = N, N, N
     a = torch.randn(M, K, dtype=torch.float16, device="cuda:0")
     b = torch.randn(K, N, dtype=torch.float16, device="cuda:0")
 
@@ -80,6 +106,7 @@ def benchmark(N: int, provider: Literal["torch"] | Literal["triton"]):
         return perf(triton.testing.do_bench(lambda: matmul(a, b)))
 
 def test_matmul(N: int):
+    M, N, K = N, N, N
     a = torch.randn(M, K, dtype=torch.float16, device="cuda:0")
     b = torch.randn(K, N, dtype=torch.float16, device="cuda:0")
 
@@ -87,7 +114,5 @@ def test_matmul(N: int):
 
 if __name__ == "__main__":
     N = 4096
-    M, N, K = N, N, N
-
     test_matmul(N)
     benchmark.run(print_data=True, save_path=".")
